77ff6bb714a80e0d8e37bebb8d500849a5c25bb1
thaisndc <thais.12_@hotmail.com>
2024-05-04 10:29:08 -0300

    Add project files.

---
+++ ProgramaçãoOrientadaObjetos/Program.cs
@@ -0,0 +1,60 @@
+﻿#region Herança
+using System;
+
+// Classe base Animal
+class Animal
+{
+    protected string nome;
+
+    public Animal(string nome)
+    {
+        this.nome = nome;
+    }
+
+    public virtual void FazerSom()
+    {
+        Console.WriteLine("Animal faz algum som!");
+    }
+}
+
+// Classe derivada Cachorro
+class Cachorro : Animal
+{
+    public Cachorro(string nome) : base(nome)
+    {
+    }
+
+    public override void FazerSom()
+    {
+        Console.WriteLine("Au au!");
+    }
+}
+
+// Classe derivada Gato
+class Gato : Animal
+{
+    public Gato(string nome) : base(nome)
+    {
+    }
+
+    public override void FazerSom()
+    {
+        Console.WriteLine("Miau!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando objetos
+        Animal rex = new Cachorro("Rex");
+        Animal garfield = new Gato("Garfield");
+
+        // Chamando métodos
+        rex.FazerSom();
+        garfield.FazerSom();
+    }
+}
+
+#endregion
